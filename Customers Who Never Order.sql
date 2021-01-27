48a83c1991464d3d577810d4ff0495eab4b23302
Suryadeep kumar Tiwari <surya99.jin@gmail.com>
2021-01-27 09:44:49 +0530

    Add files via upload

---
+++ Customers Who Never Order.sql
@@ -0,0 +1,42 @@
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+
+##################################################################
+
+select c.Name as 'Customers' from Customers as c 
+left join Orders as o 
+on c.Id=o.CustomerId
+where o.CustomerId is null
+
+##################################################################
+
+select name as customers from 
+customers 
+where id not in (select distinct customerId from orders)
